c9466d99ebb45687cad69a811432dbb480e0b80d
Monoga <monogaalbert8@gmail.com>
2024-04-30 12:38:46 -0500

    feat: add file custom-properties

---
+++ libs/arvivo-web/scripts/build-custom-properties.js
@@ -0,0 +1,38 @@
+import { myDecisions } from '../tokens.js';
+
+import * as fs from 'fs';
+
+const transformTokens = (parentKey, object) => {
+  const objectKeys = Object.keys(object);
+
+  return objectKeys.reduce((tokensTransformed, objectKey) => {
+    const value = object[objectKey];
+
+    if (typeof value === 'object') {
+      if (parentKey === null)
+        return `${tokensTransformed}\t${transformTokens(objectKey, value)}`;
+
+      return `${tokensTransformed}\t${transformTokens(
+        `${parentKey}-${objectKey}`,
+        value
+      )}`;
+    }
+
+    return `${tokensTransformed}\n\t--${parentKey}-${objectKey}: ${value};`;
+  }, '');
+};
+
+if (!fs.existsSync('./styles')) {
+  fs.mkdirSync('./styles');
+}
+
+const data = `:root {\n\t${transformTokens(null, myDecisions).trim()}\n}`;
+if (!fs.existsSync('./styles/main.css')) {
+  fs.writeFile('./styles/main.css', data, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
+}
