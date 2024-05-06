e7fd5216e913a98697f97a94da85343952e0915d
GitHub Action <action@github.com>
2024-05-06 14:10:58 +0000

    Apply Prettier

--- libs/arvivo-react/src/stories/Label.stories.js
+++ libs/arvivo-react/src/stories/Label.stories.js
@@ -1,55 +1,56 @@
-import { Label } from "../atoms/label";
+import { Label } from '../atoms/label'
 
-export default { 
-    title:'Example/Label',
-    component:Label, 
-    tags:['autodocs'] }
+export default {
+  title: 'Example/Label',
+  component: Label,
+  tags: ['autodocs'],
+}
 
 export const ExtraSmall = {
-        args: {
-        children: 'Label x-small',
-        size: 'xs',
-        isBold: true,
-        isUpperCase: true,
-        style:{},
-        },
-    };
+  args: {
+    children: 'Label x-small',
+    size: 'xs',
+    isBold: true,
+    isUpperCase: true,
+    style: {},
+  },
+}
 
 export const Small = {
-    args: {
+  args: {
     children: 'Label Small',
     size: 'sm',
     isBold: true,
     isUpperCase: true,
-    style:{},
-    },
-};
+    style: {},
+  },
+}
 
-export const Medium ={
-    args: {
+export const Medium = {
+  args: {
     children: 'Label Medium',
     size: 'base',
     isBold: true,
     isUpperCase: true,
-    style:{},
-    },
-};
+    style: {},
+  },
+}
 
 export const Large = {
-    args: {
+  args: {
     children: 'Label Large',
     size: 'lg',
     isBold: true,
     isUpperCase: true,
-    style:{},
-    },
-};
+    style: {},
+  },
+}
 export const ExtraLarge = {
-    args: {
+  args: {
     children: 'Label ExtraLarge',
     size: 'xl',
     isBold: true,
     isUpperCase: true,
-    style:{},
-    },
-};
+    style: {},
+  },
+}
